7d20701a18b76259c3a39ba3c6220e5cf9f2d3e1
Erick Carrillo <erickcarrillo2807@gmail.com>
2021-09-24 10:51:34 -0500

    inicializador de objeto

---
+++ carros.py
@@ -0,0 +1,16 @@
+class Carros:
+    def __init__(self,marca,modelo,version,año,precio,status,dueño):
+        self.marca = marca
+        self.modelo = modelo
+        self.version = version
+        self.año = año
+        self.precio = precio
+        self.status = status
+        self.dueño = dueño
+    
+    
+
+
+
+if __name__ == "__main__":
+    pass
