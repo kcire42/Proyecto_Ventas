ef55559a3c93e5c444f74af5d6d36668c36fa813
Erick Carrillo <erickcarrillo2807@gmail.com>
2021-09-28 23:28:44 -0500

    Update ingresar datos, decimales, binarios

--- ingresar_datos.py
+++ ingresar_datos.py
@@ -25,6 +25,25 @@ class ingresar_datos:
                 break
             except ValueError:
                 print("!!!!!!ERROR SOLO SE PUEDEN INGRESAR NUMEROS¡¡¡¡¡¡")
+    
+    def ingresar_1_o_0(texto):
+        while True:
+            try:
+                numero = int(input(texto))
+                try:
+                    if numero == 1:
+                        return True
+                    elif numero == 0:
+                        return False
+                    else:
+                        raise ValueError("!!!!!!ERROR SOLO SE PUEDEN INGRESAR NUMEROS MAYORES A UNO¡¡¡¡¡¡")
+                except ValueError as solo_binarios:
+                    print(solo_binarios)
+
+            except ValueError:
+                print("!!!!!!ERROR SOLO SE PUEDEN INGRESAR NUMEROS¡¡¡¡¡¡")
+
+
 
 
 
@@ -34,3 +53,8 @@ if __name__ == '__main__':
     print(cadena)
     numero = ingresar_datos.ingresar_numeros_enteros("Ingrese un numero")
     print(numero)
+    numero_2 = ingresar_datos.ingresar_numeros_decimales("Ingresar numero con decimales: ")
+    print(numero_2)
+    binario = ingresar_datos.ingresar_1_o_0("Ingresar un 1 o 0")
+    print(binario)
+
